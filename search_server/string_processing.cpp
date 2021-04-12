9f19f0bc834fa7f79e4efedfc3aa45999dd1834c
onter7 <markovigor2@rambler.ru>
2021-04-12 20:15:39 +0300

    change string_processing.cpp: -change int64_t to size_t -add const modifier to space variable

--- search_server/string_processing.cpp
+++ search_server/string_processing.cpp
@@ -1,18 +1,19 @@
-﻿#include <cstdint>
+﻿#include <cstddef>
 
 #include "string_processing.h"
 
 std::vector<std::string_view> SplitIntoWords(std::string_view text) {
 	std::vector<std::string_view> result;
-	int64_t pos = 0;
-	const int64_t pos_end = text.npos;
+	size_t pos = 0;
+	const size_t pos_end = text.npos;
 	while (true) {
-		int64_t space = text.find(' ', pos);
-		result.push_back(space == pos_end ? text.substr(pos) : text.substr(pos, space - pos));
+		const size_t space = text.find(' ', pos);
 		if (space == pos_end) {
+			result.push_back(text.substr(pos));
 			break;
 		}
 		else {
+			result.push_back(text.substr(pos, space - pos));
 			pos = space + 1;
 		}
 	}
